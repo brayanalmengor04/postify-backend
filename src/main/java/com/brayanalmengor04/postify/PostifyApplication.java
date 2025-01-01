14a0a7607cbdf92d6133154b157a761772926caf
Brayan Almengor <brayanalmengor300@gmail.com>
2025-01-01 13:35:13 -0500

    upload initializer spring boot backend

---
+++ src/main/java/com/brayanalmengor04/postify/PostifyApplication.java
@@ -0,0 +1,13 @@
+package com.brayanalmengor04.postify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostifyApplication.class, args);
+	}
+
+}
