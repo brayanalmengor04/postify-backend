ede7150a12174de6bdba9add144b10e7f5ea62fd
Brayan Almengor <brayanalmengor300@gmail.com>
2025-02-19 13:43:25 -0500

    adding file to dockerize the app postify

---
+++ src/main/java/com/brayanalmengor04/postify/controller/IndexPrueba.java
@@ -0,0 +1,15 @@
+package com.brayanalmengor04.postify.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexPrueba {
+
+    @GetMapping("/test")  // URL: http://localhost:8080/test
+    @ResponseBody
+    public String test() {
+        return "¡Todo funciona correctamente! ";
+    }
+}
